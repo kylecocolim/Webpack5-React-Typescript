67f8153f1c44649508455824df7a45aafa37c2d1
임진택 <kococo9504@nextlab.co.kr>
2021-06-07 17:24:22 +0900

    ADD Split Chunks for JS

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,14 +1,30 @@
 const { merge } = require('webpack-merge')
-const common = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'production',
     optimization: {
         minimize: true,
-        minimizer: [new TerserPlugin({})],
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                ecma: 5,
+                mangle: true,
+                keep_fnames: false,
+            }
+        })],
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        }
     },
     plugins: [
         new CleanWebpackPlugin(),
--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './app'
 import 'style/index.scss'
 
+// 하이 Hi
 ReactDOM.render(<App />, document.getElementById('root'))
 
